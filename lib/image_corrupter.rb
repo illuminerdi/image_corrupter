bf5f1ecd9d4d760492c8763e6ff028aeaa2a8741
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2011-02-12 13:49:45 -0800

    implemented chunk size, arrays of corrupting text, random placement of glitches, custom number of occurrences

--- lib/image_corrupter.rb
+++ lib/image_corrupter.rb
@@ -1,7 +1,15 @@
 class ImageCorrupter
   VERSION = '1.0.0'
 
+  DEFAULT_OPTIONS = {
+    interval: 5000,
+    occurrences: 1,
+    custom_text: "JOSHUA OWNZ j00 ALL!!!",
+    random: false
+  }
   attr_accessor :file_name, :out_file_name, :file_bytes, :corrupted_file_bytes
+
+  Struct.new("InjectionPoint", :start, :value)
 
   def initialize(in_file, options = {})
     @file_name = in_file
@@ -13,31 +21,23 @@ class ImageCorrupter
   end
 
   def corrupt
-    temp_file_bytes = @file_bytes
-    step = 5000
-    injecting = "JOSHUA"
-    6.times {|i|
-      @corrupted_file_bytes = temp_file_bytes.fill(step*i,6) {|j|
-        injecting[j]
+    @corrupted_file_bytes.replace(@file_bytes)
+    injection_points.each {|p|
+      @corrupted_file_bytes.fill(p.start, p.value.length) {|i|
+        p.value[i % p.start]
       }
-    }
-    @corrupted_file_bytes = temp_file_bytes.fill(5000,)
-    temp_file_bytes.each_with_index {|b,i|
-      if(i > @photo[:start_byte] && !corrupted)
-        "JOSHUA".each_char {|c|
-          @corrupted_file_bytes << c
-        }
-        corrupted = true
-        temp_file_bytes.slice!(i,"JOSHUA".length-1)
-      else
-        @corrupted_file_bytes << b
-      end
     }
   end
 
   def corrupt!
     corrupt
-    @file_bytes = @corrupted_file_bytes
+    @file_bytes.replace(@corrupted_file_bytes)
+  end
+
+  def self.corrupt(in_file)
+    corrupting = ImageCorrupter.new(in_file)
+    corrupting.corrupt
+    corrupting.to_file
   end
 
   def to_file
@@ -46,31 +46,51 @@ class ImageCorrupter
         f.putc b
       }
     }
+    @out_file_name
   end
 
   private
 
-  def parse_options(options = {})
+    def injection_points
+      step = @options[:interval]
+      injecting = @options[:custom_text]
+      points = []
+      (1..@options[:occurrences]).each {|i|
+        index = @options[:random] ? random_index : step * i
+        iter_inject = @options[:custom_text].kind_of?(Array) ? injecting[(i % injecting.size)-1] : injecting
+        points << Struct::InjectionPoint.new(index, iter_inject[0...(@options[:chunk_size] || iter_inject.length)])
+      }
+      points
+    end
 
-  end
+    def random_index
+      rand(@photo[:end_of_img]-@photo[:start_byte]) + @photo[:start_byte]
+    end
 
-  def analyze_photo
-    @photo = {}
-    @photo[:restart_points] = []
-    file_bytes.each_with_index {|b,i|
-      if b == 0xFF
-        case file_bytes[i+1]
-          when 0xDD
-            @photo[:reset_byte] = [file_bytes[i+2], file_bytes[i+3]]
-          when 0xDA
-            @photo[:start_byte] = i+2
-          when 0xD0..0xD7
-            @photo[:restart_points] << i+2
-          when 0xD9
-            @photo[:end_of_img] = i+2
-        end
+    def parse_options(options = {})
+      @options = DEFAULT_OPTIONS.merge(options)
+      unless(@options[:corruption_text_file].nil?)
+
       end
-    }
-  end
+    end
+
+    def analyze_photo
+      @photo = {}
+      @photo[:restart_points] = []
+      file_bytes.each_with_index {|b,i|
+        if b == 0xFF
+          case file_bytes[i+1]
+            when 0xDD
+              @photo[:reset_byte] = [file_bytes[i+2], file_bytes[i+3]]
+            when 0xDA
+              @photo[:start_byte] = i+2
+            when 0xD0..0xD7
+              @photo[:restart_points] << i+2
+            when 0xD9
+              @photo[:end_of_img] = i+2
+          end
+        end
+      }
+    end
 
 end
--- test/test_image_corrupter.rb
+++ test/test_image_corrupter.rb
@@ -41,13 +41,82 @@ class TestImageCorrupter < Test::Unit::TestCase
 
   def test_will_corrupt!
     corrupter = ImageCorrupter.new(IMAGE_TEST_FILE)
-    file_bytes = corrupter.file_bytes
+    file_bytes = []
+    file_bytes.replace(corrupter.file_bytes)
     corrupter.corrupt!
 
     assert_not_equal 0, corrupter.file_bytes.size,
     "Data not actually corrupted, size of file_bytes is 0 (default size of corrupted_file_bytes)"
     assert_equal file_bytes.size, corrupter.file_bytes.size
     assert file_bytes != corrupter.file_bytes
+  end
+
+  def test_class_corrupt_convenience_method_creates_a_corrupted_file
+    ImageCorrupter.corrupt(IMAGE_TEST_FILE)
+
+    assert File.exists?(IMAGE_TEST_FILE_OUT)
+    assert_not_equal 0, File.size(IMAGE_TEST_FILE_OUT)
+  end
+
+  def test_static_default_options_corruption_creates_the_same_corrupted_file_twice
+    idempotent = ImageCorrupter.new(IMAGE_TEST_FILE)
+    @corrupter.corrupt
+    idempotent.corrupt
+
+    assert_equal @corrupter.corrupted_file_bytes, idempotent.corrupted_file_bytes
+  end
+
+  def test_different_options_creates_two_different_corrupted_files
+    optionified = ImageCorrupter.new(IMAGE_TEST_FILE, :interval => 23_000, :occurrences => 3)
+    optionified.corrupt
+    @corrupter.corrupt
+
+    assert @corrupter.corrupted_file_bytes != optionified.corrupted_file_bytes, "Base corrupter and optionified had same result"
+  end
+
+  def test_corrupter_options_allows_array_of_custom_text
+    optionified = ImageCorrupter.new(IMAGE_TEST_FILE, :custom_text => ["JOSHUA CLINGENPEEL", "HE IS THE OWNER OF YOU"])
+    optionified.corrupt
+
+    assert_equal optionified.file_bytes.size, optionified.corrupted_file_bytes.size
+  end
+
+  def test_corrupter_options_custom_text_array_has_same_effect_as_string
+    # assumes default occurrences of 1
+    options = {
+      custom_text: ["JOSHUA OWNZ j00 ALL!!!", "JOSHUA CLINGENPEEL"]
+    }
+    optionified = ImageCorrupter.new(IMAGE_TEST_FILE, options)
+    optionified.corrupt
+    @corrupter.corrupt
+
+    assert @corrupter.corrupted_file_bytes == optionified.corrupted_file_bytes
+
+    options[:occurrences] = 2
+    optionified = ImageCorrupter.new(IMAGE_TEST_FILE, options)
+    optionified.corrupt
+
+    assert @corrupter.corrupted_file_bytes != optionified.corrupted_file_bytes
+  end
+
+  def test_corrupter_chunk_size_restricts_injection
+    options = {
+      chunk_size: 5
+    }
+
+    optionified = ImageCorrupter.new(IMAGE_TEST_FILE, options)
+    optionified.corrupt
+    @corrupter.corrupt
+
+    assert @corrupter.corrupted_file_bytes != optionified.corrupted_file_bytes
+  end
+
+  def test_random_corruption
+    randomized = ImageCorrupter.new(IMAGE_TEST_FILE, :random => true)
+    randomized.corrupt
+    @corrupter.corrupt
+
+    assert @corrupter.corrupted_file_bytes != randomized.corrupted_file_bytes
   end
 
   def test_to_file
